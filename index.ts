f7a899bd33b599f6239751ea477c159f96236ed9
Lucas Henrique <lucashenriquesd@gmail.com>
2022-07-29 18:29:50 -0300

    fix path index.html

--- index.ts
+++ index.ts
@@ -9,7 +9,7 @@ const io = new Server(server);
 let connectedUsersAmount: number = 0;
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+  res.sendFile(__dirname + '../index.html');
 });
 
 io.on('connection', (socket) => {
